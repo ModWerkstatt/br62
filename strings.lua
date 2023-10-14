0615ab853a3107eaf15d45be61241aac6c280f03
Florian Thiel <flth@flth.de>
2023-10-14 23:24:45 +0200

    überarbeitete Texte

--- strings.lua
+++ strings.lua
@@ -2,39 +2,42 @@ function data()
 return {
 		de = {
 
-			["mod_name"] = "DRG/DB BR 62",
-			["mod_desc"] = "Die Baureihe 62 wurde vom Engeren Lokomotivausschuss des Reichsbahn-Zentralamtes entwickelt und von Henschel Ende der 1920er-Jahre gebaut. Die Maschinen waren Zweizylinder-Heißdampflokomotiven. Es wurden insgesamt 15 Exemplare hergestellt. Die ersten Maschinen 62 001 und 002 waren von 1928 bis 1932 beim Bahnbetriebswerk Lennep zwischen Düsseldorf, Wuppertal, Remscheid und Solingen im Einsatz. Obwohl alle Lokomotiven bereits 1928 gebaut worden waren, nahm die Deutsche Reichsbahn die 62 003–015 erst 1932 ab. Gründe dafür waren der geringe Bedarf bei der Reichsbahn sowie der hohe Preis für die Lokomotiven. Einsatz-Bahnbetriebswerke in den 1930er-Jahren waren Düsseldorf-Abstellbahnhof, Saßnitz auf Rügen sowie Meiningen. Vor allem auf der Werrabahn von Eisenach nach Lichtenfels konnten die Loks ihre Sprintschnelligkeit ausspielen. Nach dem Zweiten Weltkrieg verblieben acht Exemplare bei der Deutschen Reichsbahn und sieben bei der Deutschen Bundesbahn.",
+			["mod_name"] = "DRG/DR/DB BR 62",
+			["mod_desc"] = "Die Baureihe 62 wurde vom Engeren Lokomotivausschuss des Reichsbahn-Zentralamtes entwickelt und von Henschel Ende der 1920er-Jahre gebaut. Die Maschinen waren Zweizylinder-Heißdampflokomotiven. Es wurden insgesamt 15 Exemplare hergestellt. Obwohl alle Lokomotiven bereits 1928 gebaut worden waren, nahm die Deutsche Reichsbahn die 62 003–015 erst 1932 ab. Gründe dafür waren der geringe Bedarf bei der Reichsbahn sowie der hohe Preis für die Lokomotiven.\nVor allem auf der Werrabahn von Eisenach nach Lichtenfels konnten die Loks ihre Sprintschnelligkeit ausspielen. Nach dem Zweiten Weltkrieg verblieben acht Exemplare bei der Deutschen Reichsbahn und sieben bei der Deutschen Bundesbahn.",
 
 			["br62_menu"] = "BR 62",
-			["br62_1_name"] = "BR 62 001 DRG",
-			["br62_1b_name"] = "BR 62 DRG",
-			["br62_2_name"] = "BR 62 DRG",
-			["br62_3_name"] = "BR 62 DR",
-			["br62_4_name"] = "BR 62 DR",
-			["br62_5_name"] = "BR 62 DB",
+			["br62_1_name"] = "BR 62 001 DRG (1928)",
+			["br62_1b_name"] = "BR 62 DRG (1932)",
+			["br62_2_name"] = "BR 62 DRG (1939)",
+			["br62_3_name"] = "BR 62 DR (1945)",
+			["br62_4_name"] = "BR 62 DR (1970)",
+			["br62_5_name"] = "BR 62 DB (1949)",
 
-			["br62_1_desc"] = "Die Baureihe 62 wurde vom Engeren Lokomotivausschuss des Reichsbahn-Zentralamtes entwickelt und von Henschel Ende der 1920er-Jahre gebaut. Die Maschinen waren Zweizylinder-Heißdampflokomotiven. Es wurden insgesamt 15 Exemplare hergestellt. Die ersten Maschinen 62 001 und 002 waren von 1928 bis 1932 beim Bahnbetriebswerk Lennep zwischen Düsseldorf, Wuppertal, Remscheid und Solingen im Einsatz. Obwohl alle Lokomotiven bereits 1928 gebaut worden waren, nahm die Deutsche Reichsbahn die 62 003–015 erst 1932 ab.",
-			["br62_2_desc"] = "Die Baureihe 62 wurde vom Engeren Lokomotivausschuss des Reichsbahn-Zentralamtes entwickelt und von Henschel Ende der 1920er-Jahre gebaut. Die Maschinen waren Zweizylinder-Heißdampflokomotiven. Es wurden insgesamt 15 Exemplare hergestellt. Die ersten Maschinen 62 001 und 002 waren von 1928 bis 1932 beim Bahnbetriebswerk Lennep zwischen Düsseldorf, Wuppertal, Remscheid und Solingen im Einsatz. Obwohl alle Lokomotiven bereits 1928 gebaut worden waren, nahm die Deutsche Reichsbahn die 62 003–015 erst 1932 ab.",
-			["br62_3_desc"] = "Zunächst waren die Lokomotiven (62 006, 007, 008, 009, 010, 012, 014 und 015) in der DDR auf verschiedene Betriebswerke verteilt, 1955 wurden sie alle in Meiningen beheimatet. 1958 bis 1959 wurden alle Lokomotiven dem Bw Berlin Ostbahnhof zugewiesen, wo sie vor allem mit den Sputnik (Eisenbahn)-Zügen auf dem Berliner Außenring eingesetzt wurden. Anfang der 1960er Jahre kamen sie nach Schwerin und Rostock, wo sie in den Jahren 1965–1967 Doppelstock-Wendezüge von und nach Warnemünde bespannten. Einen kurzen Aufenthalt hatten die Maschinen auch im Bw Wittenberge und im Bw Berlin-Lichtenberg.",
-			["br62_4_desc"] = "Anfang 1970 waren nur noch die 62 007, 014 und 015 in der Einsatzstelle Wriezen im Einsatz, wo sie Züge nach Berlin-Lichtenberg bespannten. Die 62 007 wurde dort als letzte planmäßig genutzte Lok, die auch als einzige die EDV-Nummer 62 1007-4 erhielt, 1972 ausgemustert, aber noch bis 1973 als Heizlok genutzt.",
-			["br62_5_desc"] = "Die Deutsche Bundesbahn hatte ihre Lokomotiven (62 001, 002, 003, 004, 005, 011 und 013), die großenteils bei Kriegsende nicht betriebsfähig waren und erst 1949 hauptuntersucht wurden, zunächst in Wuppertal, später in Dortmund, Düsseldorf, Essen und Krefeld beheimatet. Bis 1956 wurden die Lokomotiven bei der Deutschen Bundesbahn abgestellt. Die letzte Lok, 62 003, wurde 1972 in Mülheim an der Ruhr verschrottet, nachdem sie von 1956 bis 1966 noch als Lehrmodell in der Lokführerschule in Troisdorf gedient hatte und anschließend mit anderen schrottreifen Lokomotiven im AW Schwerte aufbewahrt worden war.",
+			["br62_1_desc"] = "Die Baureihe 62 wurde vom Engeren Lokomotivausschuss des Reichsbahn-Zentralamtes entwickelt und von Henschel Ende der 1920er-Jahre gebaut. Die Maschinen waren Zweizylinder-Heißdampflokomotiven. Es wurden insgesamt 15 Exemplare hergestellt. Obwohl alle Lokomotiven bereits 1928 gebaut worden waren, nahm die Deutsche Reichsbahn die 62 003–015 erst 1932 ab.",
+			["br62_2_desc"] = "Die Baureihe 62 wurde vom Engeren Lokomotivausschuss des Reichsbahn-Zentralamtes entwickelt und von Henschel Ende der 1920er-Jahre gebaut. Die Maschinen waren Zweizylinder-Heißdampflokomotiven. Es wurden insgesamt 15 Exemplare hergestellt. Obwohl alle Lokomotiven bereits 1928 gebaut worden waren, nahm die Deutsche Reichsbahn die 62 003–015 erst 1932 ab.",
+			["br62_3_desc"] = "Zunächst waren die Lokomotiven in der DDR auf verschiedene Betriebswerke verteilt, 1955 wurden sie alle in Meiningen beheimatet. 1958 bis 1959 wurden alle Lokomotiven dem Bw Berlin Ostbahnhof zugewiesen, wo sie vor allem mit den Sputnik (Eisenbahn)-Zügen auf dem Berliner Außenring eingesetzt wurden. Anfang der 1960er Jahre kamen sie nach Schwerin und Rostock, wo sie in den Jahren 1965–1967 Doppelstock-Wendezüge von und nach Warnemünde bespannten. Einen kurzen Aufenthalt hatten die Maschinen auch im Bw Wittenberge und im Bw Berlin-Lichtenberg.",
+			["br62_4_desc"] = "Anfang 1970 waren nur noch drei Lokomotiven in der Einsatzstelle Wriezen im Einsatz, wo sie Züge nach Berlin-Lichtenberg bespannten. Die 62 007 wurde dort als letzte planmäßig genutzte Lok, die auch als einzige die EDV-Nummer 62 1007-4 erhielt, 1972 ausgemustert, aber noch bis 1973 als Heizlok genutzt.",
+			["br62_5_desc"] = "Die Deutsche Bundesbahn hatte ihre Lokomotiven, die großenteils bei Kriegsende nicht betriebsfähig waren  erst ab 1949 einer Hauptuntersuchung zugeführt. Bis 1956 wurden die Lokomotiven bei der Deutschen Bundesbahn abgestellt. Die letzte Lok wurde 1972 in Mülheim an der Ruhr verschrottet, nachdem sie bis 1966 noch als Lehrmodell in der Lokführerschule in Troisdorf gedient hatte und anschließend mit anderen schrottreifen Lokomotiven im AW Schwerte aufbewahrt worden war.",
 
 			},
 		en = {
 
-			["br62_menu"] = "Class 62",
-			["br62_1_name"] = "Class 62 001 DRG",
-			["br62_1b_name"] = "Class 62 DRG",
-			["br62_2_name"] = "Class 62 DRG",
-			["br62_3_name"] = "Class 62 DR",
-			["br62_4_name"] = "Class 62 DR",
-			["br62_5_name"] = "Class 62 DB",
+			["mod_name"] = "DRG/DR/DB Class 62",
+			["mod_desc"] = "The class 62 was developed by the Engeren Lokomotivausschuss des Reichsbahn-Zentralamtes and built by Henschel at the end of the 1920s. The machines were two-cylinder hot steam locomotives. A total of 15 examples were produced. Although all locomotives had already been built in 1928, the Deutsche Reichsbahn did not take delivery of the 62 003-015 until 1932. The reasons for this were the low demand at the Reichsbahn as well as the high price for the locomotives.\nThe locomotives were able to show off their sprint speed especially on the Werrabahn from Eisenach to Lichtenfels. After the Second World War, eight locomotives remained with the Deutsche Reichsbahn and seven with the Deutsche Bundesbahn.",
 
-			["br62_1_desc"] = "The Class 62 was developed by the Locomotive Committee of the Reichsbahn Central Office and built by Henschel in the late 1920s. The engines were two-cylinder superheated steam locomotives. A total of 15 copies were made. The first machines 62 001 and 002 were in use from 1928 to 1932 at the Lennep railway depot between Düsseldorf, Wuppertal, Remscheid and Solingen. Although all locomotives had already been built in 1928, the Deutsche Reichsbahn did not accept the 62 003–015 until 1932.",
-			["br62_2_desc"] = "The Class 62 was developed by the Locomotive Committee of the Reichsbahn Central Office and built by Henschel in the late 1920s. The engines were two-cylinder superheated steam locomotives. A total of 15 copies were made. The first machines 62 001 and 002 were in use from 1928 to 1932 at the Lennep railway depot between Düsseldorf, Wuppertal, Remscheid and Solingen. Although all locomotives had already been built in 1928, the Deutsche Reichsbahn did not accept the 62 003–015 until 1932.",
-			["br62_3_desc"] = "Initially the locomotives (62 006, 007, 008, 009, 010, 012, 014 and 015) were distributed among different depots in the GDR; in 1955 they were all based in Meiningen. From 1958 to 1959 all locomotives were assigned to the Berlin Ostbahnhof depot, whereby they were primarily used with the Sputnik (railway) trains on the Berlin outer ring. At the beginning of the 1960s they came to Schwerin and Rostock, where they pulled double-decker push-pull trains to and from Warnemünde between 1965 and 1967. The machines also had a short stay at the Wittenberge depot and the Berlin-Lichtenberg depot.",
-			["br62_4_desc"] = "At the beginning of 1970, only the 62 007, 014 and 015 were still in use at the Wriezen operation center, where they hauled trains to Berlin-Lichtenberg. The 62 007 was the last locomotive in scheduled use there, which was also the only one given the computer number 62 1007-4, and was decommissioned in 1972, but was still used as a heating locomotive until 1973.",
-			["br62_5_desc"] = "The Deutsche Bundesbahn had its locomotives (62 001, 002, 003, 004, 005, 011 and 013), most of which were not operational at the end of the war and were not fully inspected until 1949, initially in Wuppertal and later in Dortmund, Düsseldorf, Essen and Krefeld . The locomotives were parked at the Deutsche Bundesbahn until 1956. The last locomotive, 62 003, was scrapped in Mülheim an der Ruhr in 1972 after it had served as a teaching model in the locomotive driving school in Troisdorf from 1956 to 1966 and was then stored at AW Schwerte with other locomotives that were ready for scrap.",
+			["br62_menu"] = "Class 62",
+			["br62_1_name"] = "Class 62 001 DRG (1928)",
+			["br62_1b_name"] = "Class 62 DRG (1932)",
+			["br62_2_name"] = "Class 62 DRG (1939)",
+			["br62_3_name"] = "Class 62 DR (1945)",
+			["br62_4_name"] = "Class 62 DR (1970)",
+			["br62_5_name"] = "Class 62 DB (1949)",
+
+			["br62_1_desc"] = "The class 62 was developed by the Engeren Lokomotivausschuss des Reichsbahn-Zentralamtes and built by Henschel at the end of the 1920s. The machines were two-cylinder hot steam locomotives. A total of 15 examples were produced. Although all locomotives had already been built in 1928, the Deutsche Reichsbahn did not take delivery of the 62 003-015 until 1932.",
+			["br62_2_desc"] = "The class 62 was developed by the Engeren Lokomotivausschuss des Reichsbahn-Zentralamtes and built by Henschel at the end of the 1920s. The machines were two-cylinder hot steam locomotives. A total of 15 examples were produced. Although all locomotives had already been built in 1928, the Deutsche Reichsbahn did not take delivery of the 62 003-015 until 1932.",
+			["br62_3_desc"] = "Initially, the locomotives were distributed among various depots in the GDR, and in 1955 they were all based in Meiningen. From 1958 to 1959, all locomotives were assigned to the Bw Berlin Ostbahnhof, where they were mainly used with the Sputnik (railway) trains on the Berlin outer ring. In the early 1960s they came to Schwerin and Rostock, where they crewed double-decker reversible trains to and from Warnemünde in 1965-1967. The machines also had a short stay at the Wittenberge depot and the Berlin-Lichtenberg depot.",
+			["br62_4_desc"] = "At the beginning of 1970, only three locomotives were still in service at the Wriezen depot, where they hauled trains to Berlin-Lichtenberg. The 62 007 was the last scheduled locomotive there, and the only one to be given the EDP number 62 1007-4. It was taken out of service in 1972, but was still used as a heating locomotive until 1973.",
+			["br62_5_desc"] = "The Deutsche Bundesbahn did not have its locomotives, most of which were not operational at the end of the war, undergo a general inspection until 1949. The locomotives were parked with the Deutsche Bundesbahn until 1956. The last locomotive was scrapped in 1972 in Mülheim an der Ruhr, after having served as a training model in the locomotive driver school in Troisdorf until 1966 and then being stored with other locomotives that were ready for scrap in the Schwerte works.",
 
 			},
 	}
