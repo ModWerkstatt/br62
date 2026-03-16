2fe1e282736683d13c9dc724226d5e3833968e9e
Dylan7654 <dylan.kreuzinger@gmail.com>
2026-03-16 14:04:31 +0100

    Final Convert

---
+++ vehicle/train/br62/sound/br62.snd.lua
@@ -0,0 +1,75 @@
+local soundsetutil = require "::/scripts/soundsetutil.lua"
+
+function data()
+
+local data = soundsetutil.makeSoundSet()
+
+soundsetutil.addTrackParam01
+(data, "br62/idle.wav", 25.0,
+		{ 
+		{ 0.0, 0.7 }, 
+		{ 0.05, 0.0 } },
+		{
+		{ .0, 1.0 }, 
+		{ 1.0, 1.0 } },
+		{"vehicle", "speed01"})
+		
+soundsetutil.addTrackParam01
+(data, "br62/wassern.wav", 25.0,
+		{ 
+		{ 0.0, 0.0 },
+		{ 0.01, 1.0 },  
+		{ 0.05, 1.0 }, 
+		{ 0.1, 0.0 } }, 
+		{ 
+		{ .0, 1.0 }, 
+		{ 1.0, 1.0 } },
+		{"vehicle", "speed01"})
+		
+soundsetutil.addTrackParam01
+(data, "br62/drive.wav", 25.0,
+		{ 
+		{ 0.0, 0.0 },
+		{ 0.01, 0.0 },
+		{ 0.1, 1.0 },
+		{ 0.25, 1.0 },
+		{ 0.30, 0.0 }, 
+		{ 1.0, 0.0 } },
+		{ 
+		{ 0.0, 1.2 }, 
+		{ 1.0, 1.9 } },
+		{"vehicle", "speed01"})
+		
+soundsetutil.addTrackParam01
+(data, "br62/speed.wav", 25.0,
+		{ 
+		{ 0.0, 0.0 },
+		{ 0.25, 0.0 },
+		{ 0.30, 0.8 }, 
+		{ 0.5, 2.2 },
+		{ 1.0, 2.3 } },
+		{ 
+		{ 0.0, 0.6 }, 
+		{ 1.0, 1.2 } },
+		{"vehicle", "speed01"})
+	
+soundsetutil.addTrackParam01
+(data, "br62/roll.wav", 25.0,
+		{
+			{ 0.0, 0.0 },
+			{ 0.8, 0.1 },
+			{ 1.0, 1.0 } },
+		{
+			{ 0.0, 0.5 },
+			{ 1.0, 0.5 } }, 
+		{"vehicle", "speed01"})
+		
+
+soundsetutil.addEvent(data, "horn", { "br62/horn.wav" }, 50.0)
+soundsetutil.addTrackSqueal(data, "br62/kurve.wav", 25.0)
+soundsetutil.addTrackBrake(data, "br62/bremsen.wav", 25.0, 1.0)
+
+
+return data
+
+end
